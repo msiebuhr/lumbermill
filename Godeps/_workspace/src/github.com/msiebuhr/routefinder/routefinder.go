24875f3a971dfac1152820e71dd5d236ce53d5e6
Morten Siebuhr <sbhr@sbhr.dk>
2015-08-13 14:07:12 +0200

    Update routefinder to get support for new route-styles.

--- Godeps/_workspace/src/github.com/msiebuhr/routefinder/routefinder.go
+++ Godeps/_workspace/src/github.com/msiebuhr/routefinder/routefinder.go
@@ -45,6 +45,10 @@ func NewRoutefinder(templates ...string) (Routefinder, error) {
 }
 
 func (r *Routefinder) Add(template string) error {
+	if template == "" {
+		return nil
+	}
+
 	// Quote slashes &c.
 	withQuotedMeta := regexp.QuoteMeta(template)
 
@@ -52,6 +56,10 @@ func (r *Routefinder) Add(template string) error {
 	withQuotedMeta = converterRegex.ReplaceAllStringFunc(withQuotedMeta, func(group string) string {
 		return fmt.Sprintf("(?P<%s>[^/]+)", group[1:])
 	})
+
+	if strings.HasSuffix(withQuotedMeta, "/\\.\\.\\.") {
+		withQuotedMeta = withQuotedMeta[:len(withQuotedMeta)-7] + "\\/(?P<__TRAILING__>.*)"
+	}
 
 	// Add start and end guards
 	withQuotedMeta = fmt.Sprintf("^%s$", withQuotedMeta)
@@ -112,7 +120,15 @@ func (r Routefinder) Lookup(path string) (string, map[string]string) {
 			meta[subMatchNames[j]] = subMatchValues[j]
 		}
 
-		return template.name, meta
+		templateName := template.name
+
+		// If the template ends with /... and we extracted __TRAILING__, transplant it back
+		if trailing, ok := meta["__TRAILING__"]; ok && strings.HasSuffix(template.name, "/...") {
+			templateName = templateName[:len(templateName)-3] + trailing
+			delete(meta, "__TRAILING__")
+		}
+
+		return templateName, meta
 	}
 
 	return "", map[string]string{}
